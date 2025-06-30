e58496169ec4fc4bdffb7467f37c0808a65e0398
minsub <chrkb1569@naver.com>
2025-06-30 23:48:25 +0900

    Initial Commit

---
+++ src/test/java/maeta/api/MaetaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package maeta.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaetaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
