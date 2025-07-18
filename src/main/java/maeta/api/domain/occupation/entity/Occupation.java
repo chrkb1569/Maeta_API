ef9fe48153338c0d588116c2d3fbff3da05632d2
chrkb1569 <chrkb1569@naver.com>
2025-07-18 23:21:48 +0900

    [Chore] Entity 수정 :: 데이터베이스 컬럼 형식 수정 (대문자 -> 소문자)

--- src/main/java/maeta/api/domain/occupation/entity/Occupation.java
+++ src/main/java/maeta/api/domain/occupation/entity/Occupation.java
@@ -7,20 +7,20 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Table(name = "OCCUPATION")
+@Table(name = "occupation")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Occupation {
 
     @Id
-    @Column(name = "OCCUPATION_NAME", nullable = false)
+    @Column(name = "occupation_name", nullable = false)
     private OccupationName occupationName;                  // 직업명 (히어로, 다크나이트, 팔라딘)
 
     @Enumerated(value = EnumType.STRING)
-    @Column(name = "OCCUPATION_CLASS", nullable = false)
+    @Column(name = "occupation_class", nullable = false)
     private OccupationClass occupationClass;                // 소속 (모험가, 시그너스, 레지스탕스)
 
     @Enumerated(value = EnumType.STRING)
-    @Column(name = "OCCUPATION_TYPE", nullable = false)
+    @Column(name = "occupation_type", nullable = false)
     private OccupationType occupationType;                  // 직업 분류 (전사, 궁수, 마법사)
 
     public Occupation(OccupationName occupationName, OccupationClass occupationClass, OccupationType occupationType) {
